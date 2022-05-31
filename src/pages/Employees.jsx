3e2a7be992ce68191d55399332bc9f9047b795d0
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-05-31 23:57:43 +0200

    Editing Employees Page

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, Inject, ColumnsDirective, ColumnDirective, ContextMenu, ExcelExport, PdfExport, Search, Page, Toolbar } from '@syncfusion/ej2-react-grids';
+
+import { employeesData, contextMenuItemsEmployeesPage, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
-  return (
-    <div>Employees</div>
-  )
-}
+  const toolbarOptions = ['Search'];
 
-export default Employees
+  const editing = { allowDeleting: true, allowEditing: true };
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        width="auto"
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        pageSettings={{ pageCount: 5 }}
+        editSettings={editing}
+        toolbar={toolbarOptions}
+        contextMenuItems={contextMenuItemsEmployeesPage}
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Search, Page, ExcelExport, PdfExport, ContextMenu, Toolbar]} />
+
+      </GridComponent>
+    </div>
+  );
+};
+export default Employees;
